62c4d95a6ace393e1906ad429733c6b04a3f1a92
himohimo <himohimo@gmail.com>
2024-07-21 21:49:54 +0200

    End of 7.3

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -55,8 +56,44 @@ func (m MovieModel) Insert(movie *Movie) error {
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query := `
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		WHERE id = @id
+	`
+
+	args := pgx.NamedArgs{
+		"id": id,
+	}
+
+	var movie Movie
+
+	err := m.DB.QueryRow(context.Background(), query, args).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		&movie.Genres,
+		&movie.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, pgx.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
 }
+
 func (m MovieModel) Update(movie *Movie) error {
 	return nil
 }
